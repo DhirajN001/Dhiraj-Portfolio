53132449c332873465da69ab0be2e43b44811ced
Dhiraj142d <dhirajnimbalkar142@gmail.com>
2026-07-13 19:28:16 +0530

    Portfolio Final Version

--- script.js
+++ script.js
@@ -90,57 +90,6 @@ menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
-document.getElementById("contact-form").addEventListener("submit", function(e){
-
-e.preventDefault();
-
-emailjs.sendForm(
-'YOUR_SERVICE_ID',
-'YOUR_TEMPLATE_ID',
-this
-)
-
-.then(function(){
-
-alert("Message Sent Successfully ✅");
-
-document.getElementById("contact-form").reset();
-
-},function(error){
-
-alert("Failed ❌");
-
-});
-
-});
-
-
-
-document.getElementById("contact-form").addEventListener("submit", function(e){
-
-    e.preventDefault();
-    
-    emailjs.sendForm(
-    'YOUR_SERVICE_ID',
-    'YOUR_TEMPLATE_ID',
-    this
-    )
-    
-    .then(function(){
-    
-    alert("Message Sent Successfully ✅");
-    
-    document.getElementById("contact-form").reset();
-    
-    },function(error){
-    
-    alert("Failed ❌");
-    
-    });
-    
-    });
-
-
     // Theme Toggle
 
 const themeBtn = document.getElementById("theme-toggle");
@@ -161,3 +110,189 @@ themeBtn.addEventListener("click", ()=>{
 
 });
 
+// Animated Counter
+
+const counters = document.querySelectorAll(".counter");
+
+const startCounter = () => {
+
+    counters.forEach(counter => {
+
+        const target = +counter.getAttribute("data-target");
+
+        let count = 0;
+
+        const updateCounter = () => {
+
+            const increment = target / 50;
+
+            if(count < target){
+
+                count += increment;
+
+                counter.innerText = Math.ceil(count);
+
+                setTimeout(updateCounter,20);
+
+            }
+
+            else{
+
+                counter.innerText = target + "+";
+
+            }
+
+        }
+
+        updateCounter();
+
+    });
+
+}
+
+const aboutSection = document.querySelector("#about");
+
+let counterStarted = false;
+
+window.addEventListener("scroll",()=>{
+
+    if(counterStarted) return;
+
+    const sectionTop = aboutSection.getBoundingClientRect().top;
+
+    if(sectionTop < window.innerHeight-150){
+
+        startCounter();
+
+        counterStarted = true;
+
+    }
+
+});
+
+// Scroll Progress Bar
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop =
+        document.documentElement.scrollTop;
+
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress =
+        (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width =
+        progress + "%";
+
+});
+
+// Loader
+
+window.addEventListener("load",()=>{
+
+    setTimeout(()=>{
+    
+    document.getElementById("loader").style.opacity="0";
+    
+    document.getElementById("loader").style.visibility="hidden";
+    
+    },1200);
+    
+    });
+
+    tsParticles.load("particles-js",{
+
+        background:{
+        color:"#0f172a"
+        },
+        
+        fpsLimit:60,
+        
+        particles:{
+        
+        number:{
+        value:45
+        },
+        
+        color:{
+        value:"#3b82f6"
+        },
+        
+        shape:{
+        type:"circle"
+        },
+        
+        opacity:{
+        value:0.3
+        },
+        
+        size:{
+        value:3
+        },
+        
+        move:{
+        enable:true,
+        speed:1,
+        direction:"none",
+        outModes:{
+        default:"bounce"
+        }
+        },
+        
+        links:{
+        enable:true,
+        distance:150,
+        color:"#3b82f6",
+        opacity:0.25,
+        width:1
+        }
+        
+        },
+        
+        detectRetina:true
+        
+        });
+
+        emailjs.init("8St7IPwxSRebL5ouz");
+
+const form = document.getElementById("contact-form");
+const status = document.getElementById("status");
+const sendBtn = document.getElementById("sendBtn");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    sendBtn.innerHTML = "Sending...";
+    sendBtn.disabled = true;
+
+    emailjs.sendForm(
+        "service_22wolgb",
+        "template_79b098e",
+        this
+    )
+
+    .then(() => {
+
+        status.innerHTML = "✅ Message Sent Successfully!";
+        status.style.color = "#3b82f6";
+
+        form.reset();
+
+        sendBtn.innerHTML = "Send Message";
+        sendBtn.disabled = false;
+
+    })
+
+    .catch((error) => {
+
+        console.log(error);
+    
+        alert(error.text);
+    
+    });
+
+});
